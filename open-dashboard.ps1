34c3a7e7cbca56f3748482ed5a5c6f2f97cde37c
Trương Chiến Nguyên <nguyentuong1268@gmail.com>
2026-09-04 09:03:25 +0700

    remove unused file

--- open-dashboard.ps1
+++
@@ -1,4 +0,0 @@
-# Script mo giao dien Console Dashboard test trong browser
-$dashboardPath = Join-Path $PSScriptRoot "dashboard\index.html"
-Write-Host "Opening Distributed Ticket Booking Dashboard: $dashboardPath" -ForegroundColor Cyan
-Start-Process $dashboardPath
